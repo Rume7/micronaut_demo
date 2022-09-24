1ed6c114a1cf186599027ca271cc0b176a21cfab
Rume7 <electengrhume@gmail.com>
2022-09-24 23:16:52 +0100

    Added repository

--- src/main/java/com/codehacks/repository/PersonRepository.java
+++ src/main/java/com/codehacks/repository/PersonRepository.java
@@ -1,2 +1,9 @@
-package com.codehacks.model;public interface PersonRepository {
+package com.codehacks.repository;
+
+import com.codehacks.model.Person;
+import io.micronaut.data.annotation.Repository;
+import io.micronaut.data.repository.CrudRepository;
+
+@Repository
+public interface PersonRepository extends CrudRepository<Person, Long> {
 }
